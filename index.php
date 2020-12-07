f2796b0b750c6e89325d8d25bde1cc9f061d71e6
Nathaelle <scriptales.clients@gmail.com>
2020-12-07 13:34:58 +0100

    Correction templates HTML + Routes

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 
 // index.php?page=produit&art=3
 
@@ -17,10 +19,19 @@ switch($page) {
     break;
     case "produit" : $template = "produit.php";
     break;
+    case "connexion" : $template = "connexion.php";
+    break;
+    case "panier" : $template = "panier.php";
+    break;
     default : $template = "produits.php";
 }
 
 
+function ajoutPanier() {
+    
+}
+
+//$template = "panier.php";
 ?>
 
 <!DOCTYPE html>
@@ -34,7 +45,7 @@ switch($page) {
 <body>
     
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">LOGO</a>
+        <a class="navbar-brand" href="#"><img src="img/lavande.png" alt=""> MADE IN PROVENCE</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -42,10 +53,14 @@ switch($page) {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Tous nos produits <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="index.php?page=produits">Tous nos produits <span class="sr-only">(current)</span></a>
                 </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
+
+                <a class="nav-link" href="index.php?page=connexion">Connexion <span class="sr-only"></span></a>
+                <a class="nav-link" href="index.php?page=panier">Mon panier <span class="sr-only"></span></a>
+
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
---
+++ templates/panier.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <h2>Bonjour M/Mme Dupont</h2>
+    <hr>
+    <h3>Votre panier :</h3>
+    <table class="table table-bordered">
+        <thead>
+            <tr class="bg-primary text-white">
+                <th scope="col">Référence</th>
+                <th scope="col">Désignation</th>
+                <th scope="col">Qté</th>
+                <th scope="col">Prix unitaire</th>
+                <th scope="col">Prix total</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, esse.</td>
+                <td>1</td>
+                <td>10.90</td>
+                <td>10.90</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Fuga omnis exercitationem soluta optio architecto quos tenetur?</td>
+                <td>1</td>
+                <td>15.40</td>
+                <td>15.40</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Iste vitae exercitationem magnam non! Sapiente molestias dolor aliquid. </td>
+                <td>2</td>
+                <td>8.40</td>
+                <td>16.80</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
